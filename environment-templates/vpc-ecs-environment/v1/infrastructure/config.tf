90d8afa674c2216f77b3b3f287f09a9f46a8c3e8
Mike Wrighton <mikewr@amazon.com>
2022-03-30 13:42:00 -0400

    Add tags.

--- environment-templates/vpc-ecs-environment/v1/infrastructure/config.tf
+++ environment-templates/vpc-ecs-environment/v1/infrastructure/config.tf
@@ -12,6 +12,11 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = var.aws_region
+  default_tags {
+    tags = {
+      environment = var.environment.name
+    }
+  }
 }
 
 variable "aws_region" {
