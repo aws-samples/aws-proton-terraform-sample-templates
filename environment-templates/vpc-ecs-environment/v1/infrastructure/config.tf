3682dff4674e96e4bdef72cec983a530e9fd534d
Jonathan James <jamesazj@amazon.com>
2022-04-04 14:08:53 -0400

    Add load-balanced-fargate-service infrastructure and pipeline

---
+++ environment-templates/vpc-ecs-environment/v1/infrastructure/config.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {}
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
---
+++ service-templates/apigw-lambda-svc/v1/pipeline_infrastructure/config.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    region = "us-east-1"
+    bucket = "terraform-samples-259837135622-apigw-lambda-svc"
+    key    = "pipeline.tfstate"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = data.aws_region.current.id
+  alias  = "default"
+
+  default_tags {
+    tags = {
+      "proton:pipeline" = var.service.name
+    }
+  }
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
