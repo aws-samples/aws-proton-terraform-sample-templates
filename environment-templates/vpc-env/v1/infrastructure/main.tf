16695e0088776c57e6c5a97c3a7c79c2d269654a
Ryan Racicot <racicot@amazon.com>
2022-04-08 14:34:42 -0400

    remove zipped lambda code

--- environment-templates/vpc-env/v1/infrastructure/main.tf
+++ environment-templates/vpc-env/v1/infrastructure/main.tf
@@ -3,16 +3,30 @@ module "vpc" {
 
   cidr = var.environment.inputs.vpc_cidr
 
-  azs                = ["${var.aws_region}a"]
-  private_subnets    = [var.environment.inputs.private_subnet_one_cidr, var.environment.inputs.private_subnet_two_cidr]
-  public_subnets     = [var.environment.inputs.public_subnet_one_cidr, var.environment.inputs.public_subnet_two_cidr]
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+  azs                  = ["${var.aws_region}a", "${var.aws_region}b"]
+  private_subnets      = [
+    var.environment.inputs.private_subnet_one_cidr,
+    var.environment.inputs.private_subnet_two_cidr
+  ]
+  public_subnets       = [var.environment.inputs.public_subnet_one_cidr, var.environment.inputs.public_subnet_two_cidr]
+  enable_nat_gateway   = true
+  enable_vpn_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
     Terraform   = "true"
     Environment = var.environment.name
   }
+}
+
+resource "aws_vpc_endpoint" "ec2" {
+  service_name        = "com.amazonaws.${local.region}.sns"
+  vpc_id              = module.vpc.vpc_id
+  private_dns_enabled = true
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = [module.vpc.default_security_group_id]
+  subnet_ids          = module.vpc.public_subnets
 }
 
 resource "aws_apprunner_vpc_connector" "connector" {
