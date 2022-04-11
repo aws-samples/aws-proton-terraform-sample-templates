c28637ae2d0fce9629a3ea774040d5958591556b
Jonathan James <jamesazj@amazon.com>
2022-04-11 12:40:02 -0400

    Add load-balanced-fargate-service infrastructure

---
+++ service-templates/load-balanced-fargate-svc/v1/instance_infrastructure/config.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    region = "us-east-1"
+    bucket = "terraform-states-858487653465"
+    key    = "fargate-env"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.aws_region
+  default_tags {
+    tags = {
+      "proton:service" : var.service.name
+    }
+  }
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
