865376c483124be76cb398b386493b7bd78b88e0
Aaron Wishnick <aaronwishnick@gmail.com>
2022-04-19 10:55:24 -0700

    Update vpc.tf

--- lambda-vpc/sample-vpc-environment-template/v1/infrastructure/vpc.tf
+++ lambda-vpc/sample-vpc-environment-template/v1/infrastructure/vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = var.environment.inputs.vpc_name
-  cidr = "10.0.0.0/16_bad"
+  cidr = "10.0.0.0/16"
 
   azs             = ["${var.aws_region}a"]
   private_subnets = ["10.0.1.0/24"]
