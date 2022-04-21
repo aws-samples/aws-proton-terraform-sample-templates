472771527d2fefd5e302f67bc30c58f92e240926
Mike Wrighton <mikewr@amazon.com>
2022-04-21 14:44:12 -0400

    Tidy up.

--- service-templates/apigw-lambda-svc/v1/instance_infrastructure/config.tf
+++ service-templates/apigw-lambda-svc/v1/instance_infrastructure/config.tf
@@ -6,11 +6,7 @@ terraform {
     }
   }
 
-  backend "s3" {
-    region = "us-east-1"
-    bucket = "terraform-samples-259837135622-apigw-lambda-svc"
-    key    = "instance.tfstate"
-  }
+  backend "s3" {}
 }
 
 # Configure the AWS Provider
