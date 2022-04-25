3f264c11cb1c749bfda8a43bfc66c6465995552d
Ryan Racicot <racicot@amazon.com>
2022-04-25 16:30:38 -0400

    Fix output vars format

--- environment-templates/vpc-env/v1/infrastructure/outputs.tf
+++ environment-templates/vpc-env/v1/infrastructure/outputs.tf
@@ -1,35 +1,35 @@
-output "vpc_id" {
+output "VpcId" {
   value = module.vpc.vpc_id
 }
 
-output "public_subnet_one_id" {
+output "PublicSubnetOneId" {
   value = module.vpc.public_subnets[0]
 }
 
-output "public_subnet_two_id" {
+output "PublicSubnetTwoId" {
   value = module.vpc.public_subnets[1]
 }
 
-output "private_subnet_one_id" {
+output "PrivateSubnetOneId" {
   value = module.vpc.private_subnets[0]
 }
 
-output "private_subnet_two_id" {
+output "PrivateSubnetTwoId" {
   value = module.vpc.private_subnets[1]
 }
 
-output "vpc_default_security_group" {
+output "VpcDefaultSecurityGroupId" {
   value = module.vpc.default_security_group_id
 }
 
-output "vpc_connector_arn" {
+output "VpcConnectorArn" {
   value = aws_apprunner_vpc_connector.connector.id
 }
 
-output "sns_topic_name" {
+output "SnsTopicName" {
   value = aws_sns_topic.ping_topic.name
 }
 
-output "sns_region" {
+output "SnsRegion" {
   value = local.region
 }
