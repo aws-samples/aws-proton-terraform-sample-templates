9101c4e7bdf193cdbbcafb0a63ac1d68ef959611
rayalavi <87743074+rayalavi@users.noreply.github.com>
2022-04-25 15:20:56 -0400

    Update locals.tf

--- service-templates/load-balanced-fargate-svc/v1/pipeline_infrastructure/locals.tf
+++ service-templates/load-balanced-fargate-svc/v1/pipeline_infrastructure/locals.tf
@@ -1,6 +1,4 @@
 locals {
-  account_id              = data.aws_caller_identity.current.account_id
-  region                  = data.aws_region.current.id
-  environment_account_ids = split(",", var.pipeline.inputs.environment_account_ids)
+  account_id = data.aws_caller_identity.current.account_id
+  region     = data.aws_region.current.id
 }
-
