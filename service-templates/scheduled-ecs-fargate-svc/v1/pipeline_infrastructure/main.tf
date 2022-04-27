418b040ac6dec3e189f242384123f9c67f80f841
rayalavi <87743074+rayalavi@users.noreply.github.com>
2022-04-27 09:47:45 -0400

    Update main.tf

--- service-templates/scheduled-ecs-fargate-svc/v1/pipeline_infrastructure/main.tf
+++ service-templates/scheduled-ecs-fargate-svc/v1/pipeline_infrastructure/main.tf
@@ -109,7 +109,7 @@ EOF
 resource "aws_codebuild_project" "deploy_project" {
   for_each = { for instance in var.service_instances : instance.name => instance }
 
-  name         = "${var.service.name}-deploy-${index(var.service_instances, each.value)}-project"
+  name         = "deploy-${var.service.name}-${index(var.service_instances, each.value)}"
   service_role = aws_iam_role.deployment_role.arn
 
   artifacts {
@@ -311,18 +311,18 @@ resource "aws_codepipeline" "pipeline" {
     for_each = toset(var.service_instances)
 
     content {
-      name = "Deploy${index(var.service_instances, stage.value)}Project"
+      name = "deploy-${var.service.name}-${index(var.service_instances, stage.value)}"
 
       action {
         category  = "Build"
-        name      = "Deploy${index(var.service_instances, stage.value)}"
+        name      = "deploy-${var.service.name}-${index(var.service_instances, stage.value)}"
         owner     = "AWS"
         provider  = "CodeBuild"
         version   = "1"
         run_order = 1
 
         configuration = {
-          ProjectName = "${var.service.name}-deploy-${index(var.service_instances, stage.value)}-project"
+          ProjectName = "deploy-${var.service.name}-${index(var.service_instances, stage.value)}"
         }
         input_artifacts = ["BuildOutput"]
         role_arn        = aws_iam_role.pipeline_deploy_codepipeline_action_role.arn
