0c456df139a217ac8e9b4f0cae97c4b4e8126521
Jonathan James <jamesazj@amazon.com>
2022-04-27 12:45:25 -0400

    Add Apprunner svc

---
+++ service-templates/apprunner-svc/v1/instance_infrastructure/data.tf
@@ -0,0 +1,44 @@
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_partition" "current" {}
+
+variable "task_size_cpu" {
+  type = map(string)
+  default = {
+    "medium" = "1 vCPU"
+    "large"  = "2 vCPU"
+  }
+}
+
+variable "task_size_memory" {
+  type = map(string)
+  default = {
+    "medium" = "2048"
+    "large"  = "4096"
+  }
+}
+
+data "aws_iam_policy_document" "publish_2_sns" {
+  statement {
+    actions   = ["sts:AssumeRole"]
+    effect    = "Allow"
+    resources = [var.environment.outputs.SnsTopicArn]
+
+  }
+}
+
+data "aws_iam_policy_document" "service_access_role_default_policy" {
+  statement {
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage"
+    ]
+    effect    = "Allow"
+    resources = ["*"]
+
+  }
+}
