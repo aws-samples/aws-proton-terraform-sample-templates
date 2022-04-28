fc9b7028ccf2581369cd5eb4ec28b54d187b6b9d
Jonathan James <jamesazj@amazon.com>
2022-04-28 15:34:04 -0400

    Add iam policy attachment for service role

--- service-templates/apprunner-svc/v1/instance_infrastructure/main.tf
+++ service-templates/apprunner-svc/v1/instance_infrastructure/main.tf
@@ -21,9 +21,15 @@ resource "aws_iam_role_policy" "publish_2_sns_role_policy" {
   role   = aws_iam_role.service_access_role.id
 }
 
+resource "aws_iam_role_policy_attachment" "publish_2_sns_role_policy_attachment" {
+  policy_arn = aws_iam_policy.publish_2_sns_role_policy.arn
+  role       = aws_iam_role.service_access_role.name
+}
+
 resource "aws_iam_policy" "service_access_role_default_policy" {
   name   = "service_access_role_default_policy"
   policy = data.aws_iam_policy_document.service_access_role_default_policy.json
+  role   = aws_iam_role.service_access_role.id
 }
 
 resource "aws_iam_role_policy_attachment" "service_access_role_default_policy" {
