c7eb2d781cc8fc0835395947b52c7d2b411bd18b
rayalavi <87743074+rayalavi@users.noreply.github.com>
2022-04-28 12:37:56 -0400

    Update main.tf

--- service-templates/load-balanced-ecs-ec2-svc/v1/instance_infrastructure/main.tf
+++ service-templates/load-balanced-ecs-ec2-svc/v1/instance_infrastructure/main.tf
@@ -21,7 +21,7 @@ resource "aws_security_group_rule" "lb_sg_egress" {
   to_port           = 80
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.service_security_group.id
+  security_group_id = aws_security_group.lb_sg.id
 }
 
 resource "aws_lb" "service_lb" {
