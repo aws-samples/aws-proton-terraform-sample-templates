1e4d7d2381996fdb463a7c4ad3f59f2123d4f24d
Jonathan James <jamesazj@amazon.com>
2022-04-28 15:55:39 -0400

    Update iam roles and policy

--- service-templates/apprunner-svc/v1/instance_infrastructure/main.tf
+++ service-templates/apprunner-svc/v1/instance_infrastructure/main.tf
@@ -16,9 +16,7 @@ resource "aws_iam_role" "service_access_role" {
 }
 
 resource "aws_iam_role_policy" "publish_2_sns_role_policy" {
-  name   = "publish_2_sns_role_policy"
   policy = data.aws_iam_policy_document.publish_2_sns.json
-  role   = aws_iam_role.service_access_role.id
 }
 
 resource "aws_iam_role_policy_attachment" "publish_2_sns_role_policy_attachment" {
@@ -27,12 +25,10 @@ resource "aws_iam_role_policy_attachment" "publish_2_sns_role_policy_attachment"
 }
 
 resource "aws_iam_policy" "service_access_role_default_policy" {
-  name   = "service_access_role_default_policy"
   policy = data.aws_iam_policy_document.service_access_role_default_policy.json
-  role   = aws_iam_role.service_access_role.id
 }
 
-resource "aws_iam_role_policy_attachment" "service_access_role_default_policy" {
+resource "aws_iam_policy_attachment" "service_access_role_default_policy_attachment" {
   policy_arn = aws_iam_policy.service_access_role_default_policy.arn
   role       = aws_iam_role.service_access_role.name
 }
