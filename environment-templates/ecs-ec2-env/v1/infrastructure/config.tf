c9d98c31cd3174e420dae547276ad6d46e562cbf
Ryan Racicot <racicot@amazon.com>
2022-04-29 15:42:34 -0400

    Update ecs-ec2-env outputs format

--- environment-templates/ecs-ec2-env/v1/infrastructure/config.tf
+++ environment-templates/ecs-ec2-env/v1/infrastructure/config.tf
@@ -14,12 +14,12 @@ provider "aws" {
   region = var.aws_region
   default_tags {
     tags = {
-      proton:environment = var.environment.name
+      environment = var.environment.name
     }
   }
 }
 
 variable "aws_region" {
   type    = string
-  default = "us-east-1"
+  default = "ap-northeast-1"
 }
