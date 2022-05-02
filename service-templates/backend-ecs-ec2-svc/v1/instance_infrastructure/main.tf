48cddb8402f32dbb7826d737c56d628efc376a29
mikewrighton <mike.wrighton@googlemail.com>
2022-05-02 17:09:09 -0400

    Update main.tf

--- service-templates/backend-ecs-ec2-svc/v1/instance_infrastructure/main.tf
+++ service-templates/backend-ecs-ec2-svc/v1/instance_infrastructure/main.tf
@@ -65,7 +65,7 @@ resource "aws_ecs_task_definition" "scheduled-task-definition" {
     cpu : lookup(var.task_sizes[var.service_instance.inputs.task_size], "cpu")
     memory : lookup(var.task_sizes[var.service_instance.inputs.task_size], "memory")
   }])
-  execution_role_arn       = var.environment.outputs.ServiceTaskDefExecutionRole
+  execution_role_arn       = var.environment.outputs.ServiceTaskDefExecutionRoleArn
   family                   = "${var.service.name}_${var.service_instance.name}"
   network_mode             = "bridge"
   requires_compatibilities = ["EC2"]
